bf2326fd0fb09019b92864ad89537d16283e6855
debashis ghosh <debashisghosh@debumac.local>
2017-05-13 23:32:34 -0400

    finally working

--- template.tf
+++ template.tf
@@ -4,14 +4,14 @@ resource "aws_security_group" "http_ssh" {
   description = "Allow all egress and only http and ssh ingress"
 
   ingress {
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port       = 0
+    from_port       = 80
     to_port         = 80
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
